d66ea5bc87518cbcfceb1d749dc3a6cdc3d6fa1b
serened <serened@gmail.com>
2012-10-26 17:38:40 -0700

    updates to age function

--- charactergen.rb
+++ charactergen.rb
@@ -1,18 +1,16 @@
-##pick class randomly
-    ##pick race from class
-      ##pick age from race
-##pick gender
-##pick name from race and gender
-##print the variables
-
 class CharacterBuilder
-  attr_writer :p_class, :race, :gender, :name, :age
+  attr_writer :p_class, :race, :gender, :age
     def initialize
       p_class
       race
+      gender
+      age
     end
     def p_class
       @p_class=["rogue", "wizard", "fighter", "cleric"].sample
+    end
+    def gender
+      @gender=["male","female","trans"].sample
     end
     def race
      if @p_class=="fighter"
@@ -25,47 +23,29 @@ class CharacterBuilder
         then @race=["elf", "half-orc","human"].sample
      end
     end
-    def to_s
-        "Player: #@p_class #@race \n"
-    end
-end
-
-class ElfCharacter < CharacterBuilder
-  attr_writer :gender, :name, :age
-  def initialize(p_class, race, name, age, gender)
-    super(p_class,race)
-      @name = name
-      @age = age
-      @gender = gender
-   end
-   def gender
-    @gender=["male", "female"].sample
-    end
-    def name
-      if @gender=="male"
-        @name=["Draugnon", "Yalaenon","Aranion"].sample
-      elsif @gender=="female"
-        @name=["Eruwen", "Caeliel","Nostimra"].sample
+    def age 
+      if @race=="elf" 
+        then @age=(rand(15..150))
+      elsif @race=="human"
+        then @age=(rand(15..50))
+      elsif @race=="wizard"
+        then @age=(rand(15..130))
+      elsif @race=="gnome"
+        then @age=(rand(15..130))
+      elsif @race=="dwarf"
+        then @age=(rand(15..90))
+      elsif @race=="halfling" 
+        then @age=(rand(15..100))
+      elsif @race=="half-orc" 
+        then @age=(rand(15..120))
       end
-    end
-    def age
-      @age=(rand(120))
-    end
+    end 
     def to_s
-       super + ", #@gender, #@name, #@age \n"
+        "Player: #@gender #@p_class #@race, age #@age \n"
     end
 end
-
 
 player = CharacterBuilder.new
 print player
 
-elf = ElfCharacter.new
-print elf
-##if player.race=="elf"
-  ##then ElfCharacter.new
-  ##puts player.gender
-  ##puts player.name
-  ##puts player.age
-  ##puts player.p_class
-  ##puts player.race
+##to build: naming based on race and stat generator
