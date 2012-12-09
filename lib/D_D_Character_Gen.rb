a692cf1fde5e18885e23961c71c061d624ab6c2d
serened <serened@gmail.com>
2012-12-09 12:45:02 -0800

    functional name

--- lib/D_D_Character_Gen.rb
+++ lib/D_D_Character_Gen.rb
@@ -1,10 +1,11 @@
 class CharacterBuilder
-  attr_writer :p_class, :race, :gender, :age
+  attr_writer :p_class, :race, :gender, :age, :name
     def initialize
       p_class
       race
       gender
       age
+      name
     end
     def p_class
       @p_class=["rogue", "wizard", "fighter", "cleric"].sample
@@ -41,8 +42,46 @@ class CharacterBuilder
       end
     end 
     def to_s
-        "Hello player: You are a #{@gender} #{@race} #{@p_class}, aged #{@age}. \n"
+        "Hello #{@name}: You are a #{@gender} #{@race} #{@p_class}, aged #{@age}. \n"
     end
+end
+
+CharacterBuilder.class_eval do 
+  def name
+    if @race=="human" && @gender=="male"
+      then @name=["Xanfire","Demetri","John","Captain"].sample 
+    elsif @race=="human" && @gender=="female"
+     then @name=["Fhalynn","Gwynn","Ella","Bella"].sample
+    elsif @race=="elf" && @gender=="male"
+     then @name=["Aidan","Bijan","Cedric","Daran"].sample
+    elsif @race=="elf" && @gender=="female"
+     then @name=["Elda","Galen","Julie","Lina"].sample
+    elsif @race=="half-orc" && @gender=="male"
+    then @name=["Ig","Anon","Hagu","Tilge"].sample
+   elsif @race=="half-orc" && @gender=="female"
+     then @name=["Perge","Qa","Xuga","Yroh"].sample
+   elsif @race=="gnome" && @gender=="male"
+     then @name=["Thosun","Delster","Belili","Duerdin"].sample
+   elsif @race=="gnome" && @gender=="female"
+     then @name=["Elyryan","Cyniver","Alaellyra","Merwngwen"].sample
+   elsif @race=="dwarf" && @gender=="male"
+     then @name=["Lugnus", "Marin", "Musin", "Nomli"].sample
+   elsif @race=="dwarf" && @gender=="female"
+     then @name=["Bykila", "Fikila", "Gylan", "Nale"].sample
+   elsif @race=="halfling" && @gender=="male"
+    then @name=["Milo","Olo","Kelpi","Otho"].sample
+   elsif @race=="halfling" && @gender=="female"
+     then @name=["Ruby","Poppy","Portia","Violet"].sample
+   elsif @race=="eladrin" && @gender=="male"
+     then @name=["Aialon", "Elarahal", "Jiardem", "Laern"].sample
+   elsif @race=="eladrin" && @gender=="female"
+     then @name=["Sydri","Taris","Turue","Vacquiel"].sample
+   elsif @race=="dragonborn" && @gender=="male"
+     then @name=["Donaar","Ghesh","Heskan","Kriv"].sample
+   elsif @race=="dragonborn" && @gender=="female"
+     then @name=["Daar","HArann","Kava","Korinn"].sample
+   end
+  end
 end
 
 player = CharacterBuilder.new
